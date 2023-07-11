4249f6cb239192faeb9bff5dec7565c0e80fdc05
José Andrés <joseandreshernandezross@gmail.com>
2023-07-11 17:35:01 -0400

    Cambios realizadosd

--- src/app/forms-1/products/product.ts
+++
@@ -1,9 +0,0 @@
-export interface Product {
-    name: string; // 3li6fds4cskk3yt7fm1w
-    type: string; // 0kq8kagopdbg7nwondlb
-    price: string; // al9duapn415l80smgd0p
-    code: string; // rsmk8pq1ebcvja4whr7o
-    stock: string; // t3wugwp72exut56gx7pv
-    offer: string; // nbc2mg4zvaftyyp1njk8
-    photo: string; // 776b1omatnaow79g03qq
-}
--- src/app/forms-2/orders/order.ts
+++
@@ -1,9 +0,0 @@
-export interface Order {
-  
-    amount: string; // m9apbo0te3w7s3x2ppni
-  
-    payment_type: string; // emzvxkpj0kxfvlmxmdoi
-  
-    product: string; // mt7h7f1rvzzjvgsn14zu
-  
-}
